7669ac0e82e18c83a2327c23135348e18d1bb5c4
Enrique Munguia <enrique7mc@gmail.com>
2014-01-29 12:24:45 -0600

    Add model classes Ribbit, User, UserProfile

---
+++ Models/Ribbit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RibbitMvc.Models
+{
+    public class Ribbit
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public virtual User Author { get; set; }
+
+        public string Status { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
